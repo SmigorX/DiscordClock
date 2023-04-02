2ae09647ad2f607ab76d1a7590c0baf2b75c8976
SmigorX <94678003+SmigorX@users.noreply.github.com>
2023-04-03 01:24:57 +0200

    Working version, old commands.

--- main.py
+++ main.py
@@ -8,22 +8,28 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(intents=intents, command_prefix="!")
+
+
+async def on_ready():
+    print(f'Logged in as {bot.user}')
 
 
 @bot.command()
-async def setchannel(ctx, channel_id: int):
+async def setup(ctx, channel_id: int):
     channel = bot.get_channel(channel_id)
     if channel is None:
         await ctx.send("Invalid channel ID.")
     else:
         await ctx.send(f"Target channel set to {channel.mention}.")
         while True:
-            now = time.strftime("%H:%M EVE time", time.gmtime())
-            await channel.edit(name=now)
-            print(now)
-            print(channel)
-            await asyncio.sleep(600)
+            try:
+                now = time.strftime("%H:%M EVE time", time.gmtime())
+                await channel.edit(name=now)
+                await asyncio.sleep(600)
+            except Exception as e:
+                ctx.send(f"An error occurred: {e} \n")
+                break
 
 
 TOKEN = discord_token
